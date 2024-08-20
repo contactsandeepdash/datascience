6810f8a4dff51d3629495abf66a8aaf6f2116cc1
Sandeep Dash <contactsandeepdash@gmail.com>
2024-08-20 12:14:34 +0530

    python package creation

---
+++ general/mymath/test.py
@@ -0,0 +1,6 @@
+from datascience.general.mymath.basic import square, double
+from datascience.general.mymath.stats import mean
+
+print("4 ^ 2 = ", square(4))
+print("2 * 4 = ", double(4))
+print("Mean of [1, 2, 3, 4, 5] = ", mean([1, 2, 3, 4, 5]))
